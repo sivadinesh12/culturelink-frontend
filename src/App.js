cfa98bbb6a6531a0e42f9da33ee162a6db36526c
sivadinesh12 <157784599+sivadinesh12@users.noreply.github.com>
2024-10-26 15:40:00 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [nameInput, setNameInput] = useState("");
+  const [emailInput, setEmailInput] = useState("");
+  const [messageInput, setMessageInput] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!nameInput || !emailInput || !messageInput) {
+      alert("All fields are required");
+    }
+    setIsSubmitted(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="bg-container">
+      <form className="form-el" onSubmit={handleSubmit}>
+        <div className="input-el">
+          <label for="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            required
+            value={nameInput}
+            onChange={(e) => setNameInput(e.target.value)}
+          />
+        </div>
+        <div className="input-el">
+          <label for="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            required
+            value={emailInput}
+            onChange={(e) => setEmailInput(e.target.value)}
+          />
+        </div>
+        <div className="input-el">
+          <label for="message">Message</label>
+          <textarea
+            id="message"
+            name="message"
+            cols="6"
+            rows="6"
+            required
+            value={messageInput}
+            onChange={(e) => setMessageInput(e.target.value)}
+          ></textarea>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      <div className={`modal ${isSubmitted ? "show" : "hide"}`}>
+        <h2>Form submitted successfully!</h2>
+        <button
+          onClick={() => {
+            setIsSubmitted(false);
+            setNameInput("");
+            setEmailInput("");
+            setMessageInput("");
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Close
+        </button>
+      </div>
     </div>
   );
 }
